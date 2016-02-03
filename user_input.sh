10a8cfbc6156e5de5df53364864b5858b9b43c52
redashu <asashuthegreat4@gmail.com>
2016-02-03 13:58:50 +0530

    shell change 1

---
+++ user_input.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+#echo  "plz enter anything :  "
+read  -p  "plz enter user name :   " a
+read  -s -p  "plz enter password  :   " b
+
+echo -e  "\n"
+echo  "your given username  is $a"
+echo  "your given password  is  $b"
