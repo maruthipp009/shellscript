500f5e5c9a7f084602918828413ffca732392457
redashu <asashuthegreat4@gmail.com>
2016-02-04 12:15:19 +0530

    shell condi

---
+++ condition.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+echo  -e  "plz enter your name : \c"
+read name;
+
+if  [ $name == "vimal" ]
+then
+echo  "my  name is  redhat"
+else 
+echo  "wrong choice so i am closing this programe !!"
+
+fi
+
